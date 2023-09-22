7bb95173dffe55b847a25b60924b44bfefb16fc4
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2023-09-22 12:09:55 +0530

    Day3 code

---
+++ 2_ProgrammingLanguages/Ascii.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Printing the alphabet from a to b"<<endl;
+    for(char i=97;i<123;i++){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+    cout<<"Printing the alphabet from A to B"<<endl;
+    for(char i=65;i<91;i++){
+        cout<<i<<" ";
+    }
+    return 0;
+}
--- 2_ProgrammingLanguages/Ascii.java
+++
@@ -1,9 +0,0 @@
-// package 2_ProgrammingLanguages;
-
-public class Ascii {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 127; i++) {
-            System.out.printf("ASCII value: %d, Character: %c%n", i, (char) i);
-        }
-    }
-}
