f910b18db998668b03c18da26002938a7c588e92
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2023-10-16 08:40:50 +0530

    Day29 code

---
+++ 4_Array/Day_29InsertionSort/InsertionSortDec.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int arr[] = {7, 4, 2, 3, 5};
+
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] > arr[j - 1])
+            {
+                swap(arr[j], arr[j - 1]);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    for(int i=0;i<=4;i++){
+        cout<<arr[i];
+    }
+    return 0;
+}
