0d6f8ada4d02b5c85fd9b55dbcaa132ca4e412c9
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-08-21 20:46:38 +0530

    Segment Tree Intro

---
+++ 29_SegmentTree/Maximum.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class SegmentTree{
+    vector<int>tree;
+    vector<int>arr;
+    int n;
+    public:
+    SegmentTree(vector<int>input){
+        arr=input;
+        n=input.size();
+        tree.resize(4*n);
+        build(0,0,n-1);
+    }
+    void build(int node,int start,int end){
+        if(start==end){
+            tree[node]=arr[start];
+            return;
+        }
+        int mid=start+(end-start)/2;
+        build(2*node+1,start,mid);
+        build(2*node+2,mid+1,end);
+        tree[node]=max(tree[2*node+1],tree[2*node+2]);
+    }
+
+    int query(int l,int r){
+        return range(0,0,n-1,l,r);
+    }
+    int range(int node,int start,int end,int l,int r){
+        // fully out of range
+        if(start>r || end<l){
+            return INT_MIN;
+        }
+        // fully in the range
+        if(start>=l && end<=r){
+            return tree[node];
+        }
+        // overlapping or partially in the range
+        int mid=start+(end-start)/2;
+        return max(range(2*node+1,start,mid,l,r),range(2*node+2,mid+1,end,l,r));
+    }
+
+    void update(int node, int start, int end, int idx, int val) {
+        if (start == end) {
+            arr[idx] = val;
+            tree[node] = val;
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        if (idx <= mid) {
+            update(2 * node + 1, start, mid, idx, val);
+        } else {
+            update(2 * node + 2, mid + 1, end, idx, val);
+        }
+        tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+    }
+};
+
+int main(){
+
+    vector<int>arr;
+
+    arr.push_back(3);
+    arr.push_back(6);
+    arr.push_back(4);
+    arr.push_back(25);
+    arr.push_back(5);
+    arr.push_back(13);
+    arr.push_back(18);
+
+    SegmentTree *Tree=new SegmentTree(arr);
+    cout<<Tree->query(0,2)<<endl;
+    cout<<Tree->query(3,3)<<endl;
+    return 0;
+}
